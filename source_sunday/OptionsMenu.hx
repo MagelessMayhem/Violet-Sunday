6a32ec57686aa22b736c2b0d0ae35e66c4af474e
MagelessMayhem <107271276+MagelessMayhem@users.noreply.github.com>
2022-11-05 21:14:06 -0400

    Remove duplicate variable definitions

--- source_sunday/OptionsMenu.hx
+++ source_sunday/OptionsMenu.hx
@@ -27,8 +27,6 @@ class OptionsMenu extends MusicBeatState
 
 	public var acceptInput:Bool = true;
 
-	public static var instance:OptionsMenu;
-
 	var options:Array<OptionCatagory> = [
 		new OptionCatagory("Gameplay", [
 			new DFJKOption(controls),
@@ -76,7 +74,6 @@ class OptionsMenu extends MusicBeatState
 	private var currentDescription:String = "";
 	private var grpControls:FlxTypedGroup<Alphabet>;
 	public static var versionShit:FlxText;
-	public var acceptInput:Bool = true;
 
 
 	var currentSelectedCat:OptionCatagory;
